3313db471fc22acb2f469762d9e2699ddb2ea345
Mauro Diniz <diniz.mauroaugusto@gmail.com>
2019-03-13 20:14:53 -0300

    19_03_13
    criando 1o app do curso de dev iOS do Eric Brito no Udemy

---
+++ App 01 - IMC/IMC/IMC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Mauro Augusto Diniz on 13/03/19.
+//  Copyright © 2019 Mauro Augusto Diniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
