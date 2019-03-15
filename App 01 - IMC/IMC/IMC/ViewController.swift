6cdccdd8d40637aff6f00c6a99d16f73e5c2cb54
Mauro Diniz <diniz.mauroaugusto@gmail.com>
2019-03-14 21:23:11 -0300

    19_03_14
    implementando o app de IMC

--- App 01 - IMC/IMC/IMC/ViewController.swift
+++ App 01 - IMC/IMC/IMC/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Mark - Outlets e variaveis
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    @IBOutlet weak var viResult: UIView!
+    
+    var imc: Double = 0
+    
+    // MARK - View Lifecicle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // MARK - Actions e funções
+    @IBAction func calculate(_ sender: Any) {
+        if let peso = Double(tfWeight.text!), let altura = Double(tfHeight.text!){
+            imc = peso / (altura * altura)
+            showResults()
+        }
+    }
+    
+    func showResults() {
+        var result: String = ""
+        var image: String = ""
+        
+        switch imc {
+            case 0..<16:
+                result = "Magreza"
+                image = "magreza"
+            case 16..<18.5:
+                result = "Abaixo do Peso"
+                image = "abaixo"
+            case 18.5..<25:
+                result = "Peso Ideal"
+                image = "ideal"
+            case 25..<30:
+                result = "Sobrepeso"
+                image = "sobre"
+            default:
+                result = "Obesidade"
+                image = "obesidade"
+        }
+        
+        lbResult.text = result
+        ivResult.image = UIImage(named: image)
+        viResult.isHidden = false
+    }
+    
 }
 
