09f6ed832f0f9e5270edb0a5cfc92282b2f852ce
Mauro Diniz <diniz.mauroaugusto@gmail.com>
2019-03-30 15:18:52 -0300

    19_03_30
    finalizando o app SwiftQuiz

--- App 04 - Swift Quiz/SwiftQuiz/SwiftQuiz/QuizViewController.swift
+++ App 04 - Swift Quiz/SwiftQuiz/SwiftQuiz/QuizViewController.swift
@@ -15,7 +15,22 @@ class QuizViewController: UIViewController {
     @IBOutlet weak var lbQuestion: UILabel!
     @IBOutlet var btAnswers: [UIButton]!
     
+    var quizManager = QuizManager()
+    
     // MARK - View Lifecicle
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        viTimer.frame.size.width = view.frame.size.width
+        UIView.animate(withDuration: 60.0, delay: 0, options: .curveLinear, animations: {
+            self.viTimer.frame.size.width = 0
+        }) { (success) in
+            self.showResults()
+        }
+        
+        getNewQuiz()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,7 +39,31 @@ class QuizViewController: UIViewController {
     
     // MARK - Actions e Funções
     @IBAction func selectAnswer(_ sender: UIButton) {
+        let index = btAnswers.index(of: sender)!
+        quizManager.validateAnswer(index: index)
+        
+        getNewQuiz()
     }
     
-
+    func showResults() {
+        performSegue(withIdentifier: "resultSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let resultViewController = segue.destination as? ResultViewController {
+            resultViewController.totalAnswers = quizManager.totalAnswers
+            resultViewController.totalCorrectAnswers = quizManager.totalCorrectAnswers
+        }
+    }
+    
+    func getNewQuiz(){
+        quizManager.refreshQuiz()
+        lbQuestion.text = quizManager.question
+        for i in 0..<quizManager.options.count{
+            let option = quizManager.options[i]
+            let button = btAnswers[i]
+            
+            button.setTitle(option, for: .normal)
+        }
+    }
 }
--- App 04 - Swift Quiz/SwiftQuiz/SwiftQuiz/ResultViewController.swift
+++ App 04 - Swift Quiz/SwiftQuiz/SwiftQuiz/ResultViewController.swift
@@ -16,15 +16,23 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var lbWrong: UILabel!
     @IBOutlet weak var lbScore: UILabel!
     
+    var totalCorrectAnswers: Int = 0
+    var totalAnswers: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        lbAnswered.text = "Perguntas respondidas: \(totalAnswers)"
+        lbCorrect.text = "Perguntas corretas: \(totalCorrectAnswers)"
+        lbWrong.text = "Perguntas erradas: \(totalAnswers - totalCorrectAnswers)"
+        
+        let score = (totalCorrectAnswers * 100) / totalAnswers
+        lbScore.text = "\(score)%"
     }
     
 
     @IBAction func Close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
 
