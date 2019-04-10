20dc1ac7c875927912a74409870cba9cbdc972a0
Mauro Diniz <diniz.mauroaugusto@gmail.com>
2019-04-10 15:38:13 -0300

    19_04_10
    configurando as telas do app.

---
+++ App 07 - ComprasUSA/ComprasUSA/ComprasUSA/TaxesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaxesViewController.swift
+//  ComprasUSA
+//
+//  Created by Mauro Augusto Diniz on 10/04/19.
+//  Copyright © 2019 Mauro Augusto Diniz. All rights reserved.
+//
+
+import UIKit
+
+class TaxesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
